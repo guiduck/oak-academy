3a8bdf2ad317463bb4dea3a4dc1aa56ce2e629bf
Guilherme <guiduck02@gmail.com>
2021-09-09 22:35:21 -0300

    kinda refactored

--- geolocation-app/src/index.js
+++ geolocation-app/src/index.js
@@ -3,27 +3,50 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
+
+
+  
   constructor(props) {
     super(props);
 
-    this.state = { latitude: null, errorMessage: '' }
+    this.state = { 
+      position: {
+        latitude: null,
+        longitude: null
+      },
+      loading: false,
+      errorMessage: '' 
+    }
 
-        window.navigator.geolocation.getCurrentPosition(
+      window.navigator.geolocation.getCurrentPosition(
           (position)=> {
-            this.setState({ latitude: position.coords.latitude })
-            console.log(position)
+            this.setState({ 
+              position: {
+                latitude: position.coords.latitude, 
+                longitude: position.coords.longitude, 
+              },
+              
+            })
+            console.log(position);
         },
         (error) => {
           this.setState({ errorMessage: error.message })
         }
-        );   
+      );   
   }
+  
 
   render() {
     return (
       // <GetUserCoordinates />
+      
       <div>
-        user location is: {this.state.latitude}
+        {this.state.loading ? 
+          <div>Loading ...</div> : <div>location: { 
+            this.state.position.latitude, 
+            this.state.position.longitude 
+          }</div> 
+        }
       </div>
     );
   }      
