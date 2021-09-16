ada5abe3e7dd695050f971e1161bfcdffc9a39dc
Guilherme <guiduck02@gmail.com>
2021-09-16 15:52:55 -0300

    properly fetching api data through useEffect hook

--- ninja-blog/src/Home.js
+++ ninja-blog/src/Home.js
@@ -4,20 +4,8 @@ import UserList from './UserList';
 
 const Home = () => {
 
-  const [users, setUsers] = useState([
-    {
-      // name: '',
-      // avatar: '',
-      // role: '',
-      // tweet: '',
-      // twitter: ''
-      name: "Flavio Ludgero",
-      avatar: "https://startae.com/assets/images/team/flavio-ludgero-normal_2x-2cfa44b7.jpg",
-      role: "Founder & CEO",
-      tweet: "Service designer and longboard skate lover. Been through business consulting and digital agencies. Work with mentoring and business modeling using lenses of design.",
-      twitter: "https://twitter.com/flavioludgero"
-    }
-  ])
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState(null);
 
   // useEffect(() => {
   //   const url="https://gist.githubusercontent.com/sales/f961f967c6668c7c1c3ed565e3bf94e8/raw/71168b72d9a3a199bd1de01026b5031875f6d432/team.json"
@@ -31,9 +19,28 @@ const Home = () => {
   //   }
   // }, [])
 
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/sales/f961f967c6668c7c1c3ed565e3bf94e8/raw/71168b72d9a3a199bd1de01026b5031875f6d432/team.json"
+    )
+      .then(
+        (res) => res.json()
+      ).then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+
+  }, [])
+
+  const handleDelete = (name) => {
+    const newUsers = users.filter((user) => user.name !== name)
+    setUsers(newUsers)
+  }
+
   return(
     <div>
-      <UserList users={users} />
+      {isLoading && <div>Loading...</div> }
+      {users && <UserList users={users} handleDelete={handleDelete} />}
     </div>
   );
 
--- ninja-blog/src/UserList.js
+++ ninja-blog/src/UserList.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
-export default function UserList({ users }) {
+export default function UserList({ users, handleDelete }) {
 
   return (
     <div>
-      {users.map((user) => {
+      {users.map((user, i) => {
         return (
+        <div key={i}>
           <div>
-          <p>{user.name}</p>
-          <img src={user.avatar}></img>
-          <p>{user.role}</p>
-          <p>{user.tweet}</p>
-          <a href={user.twitter}></a>
-        </div> 
+            <h2>{user.name}</h2>
+            <img src={user.avatar}></img>
+            <p>{user.role}</p>
+            <p>{user.tweet}</p>
+            <a href={user.twitter}></a>
+          </div> 
+          {console.log(user)}
+          <button onClick={() => handleDelete(user.name)}>delete user</button>
+          {console.log(user)}
+        </div>
         )         
       })}
     </div>
